86161d4a5b9ace763e5bf42458ef3afd54aac1dc
19khaled88 <kkhaled88hasan@gmail.com>
2022-06-14 16:40:12 +0600

    update class to className

--- components/Navbar.js
+++ components/Navbar.js
@@ -88,9 +88,9 @@ const Navbar = () => {
                             
                             </div>
                             </li>
-                            <form class="form-inline my-2 my-lg-0 lg:ml-auto">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            <form className="form-inline my-2 my-lg-0 lg:ml-auto">
+                                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                             </form>
                     
                         </ul>
