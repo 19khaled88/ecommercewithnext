de5499428acf54ebf201eaa313a7d0c64f1fd509
19khaled88 <kkhaled88hasan@gmail.com>
2022-06-14 16:38:28 +0600

    update script

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+// import Script from 'next/script'
 
 class MyDocument extends Document{
     render(){
@@ -8,10 +9,10 @@ class MyDocument extends Document{
                     <meta name='description' content='Dev AT E-commerce website with next.js' />
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"></link>
                     
-                    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
-                    <script src="https://kit.fontawesome.com/d7fd680996.js"></script>
+                    <script async src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+                    <script async src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+                    <script async src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+                    <script async src="https://kit.fontawesome.com/d7fd680996.js"></script>
                 </Head>
 
                 <body>
--- pages/users/signup.js
+++ pages/users/signup.js
@@ -9,7 +9,7 @@ const signup = () => {
         <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email address</label>
           <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        
         </div>
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Password</label>
@@ -19,10 +19,7 @@ const signup = () => {
         <label htmlFor="exampleInputPassword1">Confirm Password</label>
         <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
         </div>
-        <div className="form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-        </div>
+       
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     </div>
