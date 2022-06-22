2bd9b2995b242253cf5bed6aa71e7ce087901f23
19khaled88 <kkhaled88hasan@gmail.com>
2022-06-22 22:24:40 +0600

    deleted page

--- pages/_customApp.js
+++
@@ -1,8 +0,0 @@
-import AdminLayout from "./adminLayout/adminLayout"
-export default function MyApp({ Component, pageProps }) {
-    return (
-      <AdminLayout>
-        <Component {...pageProps} />
-      </AdminLayout>
-    )
-  }
